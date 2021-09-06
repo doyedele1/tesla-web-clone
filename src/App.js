2c1eb7e34c76bfd57b1ad465e7597c742b615fdf
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-09-06 05:13:30 +0100

    info/item component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,98 @@
 import './App.css';
 import Header from './components/Header';
+import Item from './components/Item';
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app-items-container">
+        <Item
+          title="Model S"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          bgImage={ModelS}
+          leftBtnText="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnText="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model Y"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          bgImage={ModelY}
+          leftBtnText="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnText="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          bgImage={Model3}
+          leftBtnText="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnText="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          bgImage={ModelX}
+          leftBtnText="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnText="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar Panels"
+          desc="Lowest Cost Solar Panels in America"
+          descLink=""
+          bgImage={SolarPanels}
+          leftBtnText="ORDER NOW"
+          leftBtnLink=""
+          rightBtnText="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar Roof"
+          desc="Produce Clean Energy From Your Roof"
+          descLink=""
+          bgImage={SolarRoof}
+          leftBtnText="ORDER NOW"
+          leftBtnLink=""
+          rightBtnText="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc=""
+          descLink=""
+          bgImage={Accessories}
+          leftBtnText="SHOP NOW"
+          leftBtnLink=""
+          rightBtnText=""
+          rightBtnLink=""
+          twoButtons="false"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "./Button";
+import "./Item.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const Item = ({
+                title, 
+                desc, 
+                descLink, 
+                bgImage, 
+                leftBtnText, 
+                leftBtnLink, 
+                rightBtnText, 
+                rightBtnLink, 
+                twoButtons, 
+                first 
+}) => {
+    return (
+        <div 
+            className="item" 
+            style={{
+                backgroundImage: `url(${bgImage})`
+            }}
+        >
+            <div className="item-container">
+                <div className="item-text">
+                    <p>{title}</p>
+                    <div className="item-text-desc">
+                        <p>{desc}</p>
+                    </div>
+                </div>
+                <div className="item-cta">
+                    <div className="item-buttons">
+                        <Button imp="primary" text={leftBtnLink} link={leftBtnLink} />
+                        {twoButtons && (
+                            <Button imp="primary" text={rightBtnLink} link={rightBtnLink} />
+                        )}
+                    </div>
+                    {first && (
+                        <div className="item-expand">
+                            <ExpandMoreIcon />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
